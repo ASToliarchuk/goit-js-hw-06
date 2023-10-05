e375763500fe612581ff14d77de544c6a8d0fbe8
ASToliarchuk <stolyarchukast@gmail.com>
2023-10-05 19:19:11 +0200

    task2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const listIngredients = document.querySelector('#ingredients');
+// console.dir(listIngredients);
+const listElements = ingredients.map((ingredient) => {
+  
+  const elementLi = document.createElement("li"); 
+  elementLi.textContent = ingredient;
+  elementLi.classList.add("item");
+  return elementLi;
+});
+
+console.log(listElements);
+
+listIngredients.append(...listElements);
