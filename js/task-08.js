9d40d7d1090aa496f54310a591ec0c77f7e18e06
ASToliarchuk <stolyarchukast@gmail.com>
2023-10-06 19:57:30 +0200

    task8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,29 @@
+const form = document.querySelector('.login-form');
 
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+event.preventDefault();
+const {
+    elements: { email, password },
+} = event.currentTarget;
+
+    if (email.value === '' && password.value === '') {
+        return alert('Уляля. Please fill in all fields!');
+    }
+    else
+        if (email.value === '') {
+            return alert('Oh, no! Please add email!');
+        } else
+            if (password.value === '') {
+                return alert('Oh, no! Please add password!');
+            }
+
+const valueObj = {
+    email: email.value,
+    password: password.value,
+};
+
+console.log(valueObj);
+event.currentTarget.reset();
+}
