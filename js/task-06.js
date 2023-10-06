08f71d3ba8e4e490b0fe7e113af6709de119501a
ASToliarchuk <stolyarchukast@gmail.com>
2023-10-06 19:48:02 +0200

    task6

---
+++ js/task-06.js
@@ -0,0 +1,16 @@
+const lengthData = document.querySelector('input[data-length]');
+const textInput = document.querySelector("#validation-input");
+const inputLength = lengthData.dataset.length;
+
+textInput.addEventListener('blur', (text) => {
+
+    if (text.currentTarget.value.length < inputLength || text.currentTarget.value.length > inputLength) {
+        textInput.classList.remove('valid');
+        textInput.classList.add('invalid');
+    }
+    else {
+        textInput.classList.remove('invalid');
+        textInput.classList.add('valid');
+    }
+}
+)
