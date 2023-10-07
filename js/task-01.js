61835dc80b11409158b2d387da7121f622812a89
ASToliarchuk <stolyarchukast@gmail.com>
2023-10-07 17:50:29 +0200

    update task1

--- js/task-01.js
+++ js/task-01.js
@@ -7,7 +7,8 @@ console.log(`Number of categories: ${allCategories.length}`);
 // }
 
 allCategories.forEach(element => {
-  console.log(`Category: ${element.querySelector('h2').textContent}`)
-  console.log(`Elements: ${element.querySelectorAll('li').length}`)
+  console.log(`Category: ${element.firstElementChild.textContent}`)
+  console.log(`Elements: ${element.lastElementChild.children.length}`)
 });
+
 
